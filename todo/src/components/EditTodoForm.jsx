4fe8f1d2c79ca86f596222945ac199890306bcb1
Ivanna <ivanaiva063@gmail.com>
2023-06-21 17:02:34 +0300

    todo_Dotsss

--- todo/src/components/EditTodoForm.jsx
+++ todo/src/components/EditTodoForm.jsx
@@ -1,19 +1,36 @@
 import React from "react";
 import { useState } from "react";
+import { Form,Row,Col,Input,Button} from "reactstrap";
 
 
 const EditTodoForm = ({ todo, handleEdit }) => {
   const [newTitle, setNewTitle] = useState(todo.title);
 
   return (
-    <form onSubmit={(e) => handleEdit(newTitle)}>
-      <input
-        type="text"
-        value={newTitle}
-        onChange={(e) => setNewTitle(e.target.value)}
-      />
-      <button type="submit">Save</button>
-    </form>
+    <Form onSubmit={(e) => handleEdit(newTitle)}>
+      <Row className="row-cols-lg-auto g-3 align-items-center">
+        <Col>
+          <Input
+            type="text"
+            value={newTitle}
+            onChange={(e) => setNewTitle(e.target.value)}
+          />
+        </Col>
+
+        <Col>
+          <Button>Submit</Button>
+        </Col>
+      </Row>
+    </Form>
+
+    // <form onSubmit={(e) => handleEdit(newTitle)}>
+    //   <input
+    //     type="text"
+    //     value={newTitle}
+    //     onChange={(e) => setNewTitle(e.target.value)}
+    //   />
+    //   <button type="submit">Save</button>
+    // </form>
   );
 };
 
