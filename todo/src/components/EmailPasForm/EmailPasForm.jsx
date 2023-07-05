eb481b530a926e2dbe303feab12ca6f063386793
Ivanna <ivanaiva063@gmail.com>
2023-07-05 18:31:05 +0300

    todo_Dotsss

--- todo/src/components/EmailPasForm/EmailPasForm.jsx
+++ todo/src/components/EmailPasForm/EmailPasForm.jsx
@@ -5,57 +5,44 @@ import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { useState } from "react";
 import { loginSuccess } from "../../store/loginSlice";
+import { useForm } from "react-hook-form";
+
+
+
+
+
+
 
 export default function EmailPasForm() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const {
+    register,
+    //formState: { errors },
+    handleSubmit,
+  } = useForm();
 
-  const [errors, setErrors] = useState({});
-  const [errorInput, setFormError] = useState(false);
-  const [showPassword, setShowPassword] = useState(false);
-
-  const handleFormSubmit = (e) => {
-    e.preventDefault();
-
-    console.log("setEmail", email);
-    console.log("setPass", password);
-    // const email = e.target.elements.email.value;
-    // const password = e.target.elements.password.value;
-
-    // Перевірити ідентифікаційні дані
-    if (email === "admin@example.com" && password === "password") {
-      // Якщо ідентифікація пройшла успішно, викликати дію loginSuccess
-      dispatch(loginSuccess());
-      navigate("/");
-    } else {
-      setErrors({ ...errors, password: "Wrong Password" });
-      setFormError(true);
-      console.log("EMAIL NOT VALID. ", password);
-    }
+  const onSubmit = (data) => {
+    alert(JSON.stringify(data));
   };
 
-  const handleForgotPassword = () => {
-    setShowPassword(!showPassword); // Toggle the showPassword state
-  };
+
 
   return (
     <div className={styles.email_pas_form}>
       <h2>Login</h2>
-      <Form onSubmit={handleFormSubmit}>
+      <Form onSubmit={handleSubmit(onSubmit)}>
         <FormGroup>
           <Label for="exampleEmail" hidden>
             Email
           </Label>
           <Input
+          {...register('email')}
             id="exampleEmail"
             name="email"
             placeholder="Email"
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+       
           />
         </FormGroup>{" "}
         <FormGroup>
@@ -63,19 +50,17 @@ export default function EmailPasForm() {
             Password
           </Label>
           <Input
+           {...register('password')}
             id="examplePassword"
             name="password"
             placeholder="Password"
-            type={showPassword ? "text" : "password"}
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className={errorInput ? styles.errorInput : ""}
+           
           />
-          <p className={styles.error_wrong}>{errors.password}</p>
+      
         </FormGroup>{" "}
-        <div className={styles.forgotPas}>
+        {/* <div className={styles.forgotPas}>
           <span onClick={handleForgotPassword}>Forgot Password</span>
-        </div>
+        </div> */}
         <Button className={styles.customButton}>Login</Button>
       </Form>
       <p>
