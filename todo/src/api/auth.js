592d16899275569ae516abb975f85839e7904ce6
Ivanna <ivanaiva063@gmail.com>
2023-07-06 10:45:35 +0300

    todo_Dotsss

---
+++ todo/src/api/auth.js
@@ -0,0 +1,23 @@
+import { Auth } from 'aws-amplify';
+
+export const getSuccessfulResponse = (response) => ({
+    isSuccess: true,
+    data: response,
+    error: null
+});
+
+export const getErrorResponse = (error) => ({
+    isSuccess: false,
+    data: null,
+    error: error
+});
+
+export async function signIn(username, password) {
+    try {
+        const user = await Auth.signIn(username, password);
+        return getSuccessfulResponse(user);
+    } catch (error) {
+        console.log('error signing in', error);
+        return getErrorResponse(error);
+    }
+}
--- todo/src/components/EmailPasForm/EmailPasForm.jsx
+++ todo/src/components/EmailPasForm/EmailPasForm.jsx
@@ -3,67 +3,53 @@ import { Form, FormGroup, Label, Input, Button } from "reactstrap";
 import styles from "./EmailPasForm.module.css";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
+import { useForm } from "react-hook-form";
+import { signIn } from "../../api/auth";
 import { useState } from "react";
 import { loginSuccess } from "../../store/loginSlice";
-import { useForm } from "react-hook-form";
-
-import { Auth } from 'aws-amplify';
-
-const getSuccessfulResponse = (response) =>({
-  isSuccess: true,
-  data: response,
-  error: null
-})
-
-const getErrorResponse = (error) =>({
-  isSuccess: false,
-  data: null,
-  error: error
-})
-
-async function signIn(username, password) {
-  try {
-    const user = await Auth.signIn(username, password);
-    return getSuccessfulResponse(user) 
-  } catch (error) {
-    console.log('error signing in', error);
-    return getErrorResponse(error)
-  }
-}
-
-
 
 export default function EmailPasForm() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const [errorMessage, setErrorMessage] = useState("");
 
   const {
     register,
     formState: { errors },
     handleSubmit,
-    setError
+    setError,
   } = useForm({
     defaultValues: {
-      email: '',
-      password: '',
+      email: "",
+      password: "",
     },
+    mode: "onBlur",
   });
 
-  const onSubmit = async(data) => {
-    console.log(data);
-    const response = await signIn(data.email, data.password)
-    if(response.isSuccess){
-      console.log(response.data)
-    }else {
-      console.log('error', response.error)
+  const onSubmit = async (data) => {
+    console.log("data", data);
+    const response = await signIn(data.email, data.password);
+    if (response.isSuccess) {
+      dispatch(loginSuccess());
+      navigate("/");
+      console.log("response.data", response.data);
+    } else {
+      setErrorMessage(response.error.message || "An error occurred");
+      console.log("response_error", response.error);
     }
     console.log(response);
   };
 
-
-  const {ref:emailRef, ...registerEmail} = register('email')
-  const {ref:passwordRef, ...registerPassword} = register('password')
- 
+  const { ref: emailRef, ...registerEmail } = register("email", {
+    required: "Email is required",
+  });
+  const { ref: passwordRef, ...registerPassword } = register("password", {
+    required: "Password is required",
+    minLength: {
+      value: 5,
+      message: "Minimum 5 characters",
+    },
+  });
 
   return (
     <div className={styles.email_pas_form}>
@@ -73,31 +59,39 @@ export default function EmailPasForm() {
           <Label for="exampleEmail" hidden>
             Email
           </Label>
-          <Input 
+          <Input
             id="exampleEmail"
             name="email"
             placeholder="Email"
             innerRef={emailRef}
-            {...registerEmail}/>
-
+            {...registerEmail}
+          />
+          <div className={styles.error_wrong}>
+            {errors.email && <p>{errors.email.message}</p>}
+          </div>
         </FormGroup>{" "}
         <FormGroup>
           <Label for="examplePassword" hidden>
             Password
           </Label>
           <Input
-          id="password"
-          name="password"
-          placeholder="Password"
-
-          innerRef={passwordRef}
-          {...registerPassword} />
-
-
+            id="password"
+            name="password"
+            placeholder="Password"
+            innerRef={passwordRef}
+            {...registerPassword}
+          />
+          <div className={styles.error_wrong}>
+            {errors.password && <p>{errors.password.message}</p>}
+          </div>
         </FormGroup>{" "}
-        {/* <div className={styles.forgotPas}>
-          <span onClick={handleForgotPassword}>Forgot Password</span>
-        </div> */}
+        <div className={styles.error_wrong}>
+          {errorMessage && <p>{errorMessage}</p>}
+        </div>
+        <div className={styles.forgotPas}>
+          {/* <span onClick={handleForgotPassword}>Forgot Password</span> */}
+          <span>Forgot Password</span>
+        </div>
         <Button className={styles.customButton}>Login</Button>
       </Form>
       <p>
